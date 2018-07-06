a87b12130214ff381bc104886bb2f92cb56c7d1f
JDNew <chijiaduo@hotmail.com>
2018-07-06 10:57:12 +0800

    提交test5

---
+++ test5/build/src/CMakeFiles/curltest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curltest.dir/main.o"
+  "curltest.pdb"
+  "curltest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/curltest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
